793ff400d2bb6234977ddb42ed7335b22d2cb90f
pgedziorowski <pgedziorowski@e-point.pl>
2023-10-11 09:57:37 +0200

    Object encapsulating of the process

--- backend/addr_nlp.py
+++ backend/addr_nlp.py
@@ -1,74 +1,73 @@
 import spacy
 import json
 import random
-
-PIPE_NAME = "addr_label_shfl_ner"
-
-
-def load_data(file):
-    with open(file, "r", encoding="utf-8") as f:
-        data = json.load(f)
-    return data
+import string
 
 
-def init_new_pipeline():
-    nlp = spacy.blank('pl')
-    ner = nlp.create_pipe("ner")
+class AddressNer:
+    def __init__(self, data_path, pipe_path='', pipe_name='', iterations=5):
+        self.data_path = data_path
+        self.pipe_path = pipe_path
+        self.pipe_name = pipe_name or self.generate_random_string(5)
+        self.iterations = iterations
+        self.nlp = self.init_pipeline()
+        self.train_data = self.load_data()
 
-    ner.add_label("VOIVODESHIP")
-    ner.add_label("COUNTY")
-    ner.add_label("COMMUNITY")
-    ner.add_label("CITY")
-    ner.add_label("STREET")
-    ner.add_label("HOUSENUMBER")
-    ner.add_label("ZIPCODE")
+    def init_pipeline(self):
+        if self.pipe_path:
+            return spacy.load(self.pipe_path)
+        return self.init_new_pipeline(self.pipe_name)
 
-    nlp.add_pipe(ner, name=PIPE_NAME)
+    @staticmethod
+    def init_new_pipeline(pipe_name):
+        nlp = spacy.blank('pl')
+        ner = nlp.create_pipe("ner")
 
-    return nlp
+        ner.add_label("VOIVODESHIP")
+        ner.add_label("COUNTY")
+        ner.add_label("COMMUNITY")
+        ner.add_label("CITY")
+        ner.add_label("STREET")
+        ner.add_label("HOUSENUMBER")
+        ner.add_label("ZIPCODE")
+        nlp.add_pipe(ner, name=pipe_name)
 
+        return nlp
 
-def train_spacy(TRAIN_DATA, iterations, pipe_path=None, new=False):
-    if not new and pipe_path:
-        try:
-            nlp = spacy.load(pipe_path)
-        except Exception as e:
-            print(f'Pipeline {pipe_path} not exists. Creating new one.')
-            nlp = init_new_pipeline()
+    def load_data(self):
+        with open(self.data_path, "r", encoding="utf-8") as f:
+            return json.load(f)
 
-    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != pipe_path]
-    with nlp.disable_pipes(*other_pipes):
-        optimizer = nlp.begin_training()
-        random.shuffle(TRAIN_DATA)
-        train_data = TRAIN_DATA[0:10000]
-        for itn in range(iterations):
-            print(f'Starting iteration: {str(itn)}')
-            losses = {}
-            for text, annotations in train_data:
-                nlp.update(
-                    [text],
-                    [annotations],
-                    drop=0.2,
-                    sgd=optimizer,
-                    losses=losses
-                )
-            print(losses)
+    def train_spacy(self):
+        other_pipes = [pipe for pipe in self.nlp.pipe_names if pipe != self.pipe_name]
+        with self.nlp.disable_pipes(*other_pipes):
+            optimizer = self.nlp.begin_training()
+            random.shuffle(self.train_data)
+            train_data = self.train_data[0:100]
+            # train_data = TRAIN_DATA
+            for itn in range(self.iterations):
+                print(f'Starting iteration: {str(itn)}')
+                losses = {}
+                for text, annotations in train_data:
+                    self.nlp.update(
+                        [text],
+                        [annotations],
+                        drop=0.2,
+                        sgd=optimizer,
+                        losses=losses
+                    )
+                print(losses)
 
-    nlp.to_disk(pipe_path)
-    return nlp
+        self.nlp.to_disk(self.pipe_path)
+        return self.nlp
 
+    def analyze_text(self, txt):
+        doc = self.nlp(txt)
+        for ent in doc.ents:
+            print(ent.text, ent.label_)
 
-def train():
-    TRAIN_DATA = load_data("ner_address_shfld_1mln.json")
-    # random.shuffle(TRAIN_DATA)
-    trained = train_spacy(TRAIN_DATA, 5)
-
-    text_to_analyze = "Mazowieckie 16a Krzywoprzysięstwa 01-666 Smolniki"
-    analyze(text_to_analyze, trained)
-
-
-def analyze(text, model=None, model_path=''):
-    model = model or spacy.load(model_path)
-    doc = model(text)
-    for ent in doc.ents:
-        print(ent.text, ent.label_)
+    @staticmethod
+    def generate_random_string(length):
+        characters = string.ascii_letters
+        rand = ''.join(random.choice(characters) for i in range(length))
+        return f'pipes/{rand}'
--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 import warnings
-import argparse
-from backend.addr_nlp import train
+
+from backend.addr_nlp import AddressNer
 from backend.preprocessing_data import prepare_data
 
-training = True
-analyze_text = ''
 
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="NER analysis arguments")
-    parser.add_argument('-t', '--train', type=str, help='Train NER')
-    parser.add_argument('-a', '--analyze-text', type=str, help='Text NER analyze')
-    # prepare_data(1000000, 'ner_address_shfld_1mln.json')
+    # prepare_data(1000, 'ner_address_shfld_1tys.json')
     # init_nlp("addr_nlp_ner")
+    address_ner = AddressNer(data_path='data/ner_address_shfld_1tys.json')
 
-    args = parser.parse_args()
 
-    training = args.train or training
-    if not training:
-        analyze_text = args.analyze_text or analyze_text
 
-    if training:
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            train()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        address_ner.train_spacy()
